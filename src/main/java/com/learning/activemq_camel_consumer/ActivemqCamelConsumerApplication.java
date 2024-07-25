0bc5c496ed71e3919c20612a67b5e64f82f4e8f0
ratnapriya <priyaratna7@gmail.com>
2024-07-25 18:19:47 +0530

    initial commit

---
+++ src/main/java/com/learning/activemq_camel_consumer/ActivemqCamelConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.learning.activemq_camel_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqCamelConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqCamelConsumerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learning/activemq_camel_consumer/route/ActiveMQConsumerRoute.java
@@ -0,0 +1,25 @@
+package com.learning.activemq_camel_consumer.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ActiveMQConsumerRoute extends RouteBuilder {
+    private final Logger LOGGER = LoggerFactory.getLogger(ActiveMQConsumerRoute.class);
+
+    @Override
+    public void configure() throws Exception {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'configure'");
+        from("activemq:queue:myQueue")
+                .log("Received message: ${body}")
+                .process(exchange -> {
+                    String message = exchange.getIn().getBody(String.class);
+                    // Process the message
+                    LOGGER.info("Processing message: " + message);
+                });
+    }
+
+}
